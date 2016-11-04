4e660513d130c6ae62436f0f52ce43ff10bf06d8
Thomas Lansing <thompa.lansing@gmail.com>
2016-11-04 19:02:11 +0100

    Fix code code intendation and rm unused code

--- app/components/App.js
+++ app/components/App.js
@@ -2,26 +2,26 @@ var React = require('react');
 var AddItem = require('./AddItem');
 var ToDoList = require('./ToDoList');
 
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {name: 'Thompa'}
-    this.handleChange = this.handleChange.bind(this);
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: 'Thompa'};
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(event) {
-    this.setState({
-      name: event.target.value
-    })
-  }
+    handleChange(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
 
-  render() {
-    return <div>
-    <input placeholder="Your name" onChange={this.handleChange}></input>
-    <h1>Welcome {this.state.name}</h1>
-    <AddItem buttonText="Add To-Do"/>
-    </div>
-  }
-};
+    render() {
+        return <div>
+            <input placeholder="Your name" onChange={this.handleChange}/>
+            <h1>Welcome {this.state.name}</h1>
+            <AddItem buttonText="Add To-Do"/>
+        </div>
+    }
+}
 
 module.exports = App;
--- app/components/ToDoList.js
+++ app/components/ToDoList.js
@@ -1,19 +1,19 @@
 var React = require('react');
 
-class ToDoList extends React.Component{
-  constructor(props) {
-    super(props);
-  }
+class ToDoList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <ul>
-          {this.props.items.map(function(item, i){
-            return <li key={i}>{item}</li>
-          })}
-      </ul>
-    )
-  }
-};
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(function (item, i) {
+                    return <li key={i}>{item}</li>
+                })}
+            </ul>
+        )
+    }
+}
 
 module.exports = ToDoList;
