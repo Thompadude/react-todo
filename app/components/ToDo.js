a02d77af86e6ae7befc8b37871de4b2f6adb1e23
Thomas Lansing <thompa.lansing@gmail.com>
2016-11-14 17:53:37 +0100

    Add feature to save lists of lists

--- app/components/ToDo.js
+++ app/components/ToDo.js
@@ -16,11 +16,17 @@ class ToDo extends React.Component {
     handleAddItem(item) {
         if (item.length > 0) {
             var itemList = this.state.items;
-            this.state.items.push(item);
+            itemList.push(item);
             this.setState({items: itemList})
         } else {
             alert('Type something!');
         }
+    }
+
+    handleAddList() {
+        var lists = this.state.lists;
+        lists.push(this.state.items);
+        this.setState({items: [], lists: lists});
     }
 
     render() {
@@ -29,6 +35,7 @@ class ToDo extends React.Component {
                 <ToDoAdd handleAddItem={this.handleAddItem.bind(this)}/>
                 <br/>
                 <ToDoList items={this.state.items}/>
+                <ToDoListSaved lists={this.state.lists} handleAddList={this.handleAddList.bind(this)}/>
             </div>
         )
     }
--- app/components/ToDoListSaved.js
+++ app/components/ToDoListSaved.js
@@ -7,17 +7,30 @@ class ToDoListSaved extends React.Component {
         super(props);
     }
 
+    handleButtonClick() {
+        this.props.handleAddList();
+    }
+
     render() {
         return (
-            <ul>
-                {
-                    this.props.lists.map(
-                        (list) => {
-                            return <ToDoList items={list}/>
-                        }
-                    )
-                }
-            </ul>
+            <div>
+                <h1>Saved Lists</h1>
+                <ul>
+                    {
+                        this.props.lists.map(
+                            (list, i) => {
+                                return (
+                                    <li key={i}>
+                                        <p>List {i + 1}</p>
+                                        <ToDoList items={list}/>
+                                    </li>
+                                )
+                            }
+                        )
+                    }
+                </ul>
+                <button onClick={this.handleButtonClick.bind(this)}>Add above list to saved lists</button>
+            </div>
         )
     }
 }
